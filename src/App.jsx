ef6c3419bd35314dc6f288cb95684f3f44efbf24
Arman <av3838@mail.ru>
2022-11-02 20:10:27 +0400

    react query devtools

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Posts } from "./Posts";
 import "./App.css";
 import { QueryClientProvider, QueryClient } from "react-query";
-
+import {ReactQueryDevtools} from 'react-query/devtools'
 
 function App() {
 
@@ -10,10 +10,12 @@ function App() {
   return (
     // provide React Query client to App
     <QueryClientProvider  client={queryClient}>
+   
     <div className="App">
       <h1>Blog Posts</h1>
       <Posts />
     </div>
+    <ReactQueryDevtools/>
     </QueryClientProvider>
   );
 }
